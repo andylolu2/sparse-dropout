412670a3d2209f2f2819d9298688cb50fa2d9edd
Andy Lo <andylolu24@gmail.com>
2023-12-08 15:32:11 +0800

    Benchmark with caching

--- benchmark/test.py
+++ benchmark/test.py
@@ -6,19 +6,63 @@ import triton.testing
 
 from flash_dropout.functional.blockwise_dropout_matmul_triton import (
     blockwise_dropout_matmul,
+    blockwise_dsd_matmul,
+    blockwise_sdd_matmul,
 )
 from flash_dropout.functional.naive import (
     blockwise_dropout_matmul as naive_blockwise_dropout_matmul,
 )
+from flash_dropout.functional.utils import (
+    blockwise_dropout_mask,
+    mask_to_increment_table,
+)
+
+block_size = (64, 64)
+p = 0.5
+cache = {}
 
 
 def f_triton(A: torch.Tensor, B: torch.Tensor):
-    C = blockwise_dropout_matmul(A, B, (64, 64), 0.5)
+    C = blockwise_dropout_matmul(A, B, block_size, p)
     C.backward(torch.zeros_like(C))
 
 
+def f_triton_cached_mask(A: torch.Tensor, B: torch.Tensor):
+    global cache
+
+    BLOCK_M, BLOCK_K = block_size
+    if (A.shape, B.shape) not in cache:
+        mask = blockwise_dropout_mask(A, block_size, p=p)
+        fwd_table, offsets, widths = mask_to_increment_table(mask, BLOCK_K)
+        fwd_header = torch.stack((offsets, widths), dim=1)
+
+        bwd_table_1 = torch.nonzero(~mask, as_tuple=False)
+
+        bwd_table_2, offsets, widths = mask_to_increment_table(mask.T, BLOCK_M)
+        bwd_header_2 = torch.stack((offsets, widths), dim=1)
+        cache[(A.shape, B.shape)] = (
+            fwd_table,
+            fwd_header,
+            bwd_table_1,
+            bwd_table_2,
+            bwd_header_2,
+        )
+    fwd_table, fwd_header, bwd_table_1, bwd_table_2, bwd_header_2 = cache[
+        (A.shape, B.shape)
+    ]
+
+    C = blockwise_dsd_matmul(
+        A, fwd_table, fwd_header, B, BLOCK_M, BLOCK_K, scale=1 / (1 - p)
+    )
+    dC = torch.zeros_like(C)
+    dA = blockwise_sdd_matmul(dC, B.T, bwd_table_1, BLOCK_M, BLOCK_K, scale=1 / (1 - p))
+    dB = blockwise_dsd_matmul(
+        A.T, bwd_table_2, bwd_header_2, dC, BLOCK_K, BLOCK_M, scale=1 / (1 - p)
+    )
+
+
 def f_naive(A: torch.Tensor, B: torch.Tensor):
-    C = naive_blockwise_dropout_matmul(A, B, (64, 64), 0.5)
+    C = naive_blockwise_dropout_matmul(A, B, block_size, p)
     C.backward(torch.zeros_like(C))
 
 
@@ -32,9 +76,9 @@ def f_dense(A: torch.Tensor, B: torch.Tensor):
         x_names=["M", "N", "K"],
         x_vals=[i for i in range(256, 4097, 256)],
         line_arg="provider",
-        line_vals=["torch", "triton", "dense"],
-        line_names=["PyTorch", "Triton", "Dense"],
-        styles=[("green", "-"), ("blue", "-"), ("red", "-")],
+        line_vals=["torch", "triton", "triton_cached", "dense"],
+        line_names=["PyTorch", "Triton", "Triton (cached)", "Dense"],
+        styles=[("green", "-"), ("blue", "-"), ("cyan", "-"), ("red", "-")],
         ylabel="TFLOPS",
         plot_name="matmul-performance",
         args={},
@@ -44,7 +88,12 @@ def benchmark(M, N, K, provider):
     A = torch.randn((M, K), device="cuda", dtype=torch.float16, requires_grad=True)
     B = torch.randn((K, N), device="cuda", dtype=torch.float16, requires_grad=True)
 
-    fs = {"torch": f_naive, "triton": f_triton, "dense": f_dense}
+    fs = {
+        "torch": f_naive,
+        "triton": f_triton,
+        "triton_cached": f_triton_cached_mask,
+        "dense": f_dense,
+    }
 
     ms, min_ms, max_ms = triton.testing.do_bench(
         lambda: fs[provider](A, B), quantiles=[0.5, 0.05, 0.95]

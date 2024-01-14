4ebdab3b9fed838053cb00f6d6a4a92143a32918
andylolu2 <andylolu24@gmail.com>
2024-01-15 03:01:29 +0800

    Finalise experiment scripts

--- experiemnts/vit_cifar10.sh
+++ experiemnts/vit_cifar10.sh
@@ -2,31 +2,19 @@
 
 seed=0
 
-# for i in 0 0.1 0.2 0.3 0.4 0.5 0.6 0.7
-# for i in 0.5
-# do
-#     python eval/vit/train.py --config eval/vit/config.py \
-#         --config.model.dropout.p=$i \
-#         --config.model.dropout.variant="vanilla" \
-#         --config.data.name="cifar10" \
-#         --config.data.train_size=16364 \
-#         --config.seed=$seed
-
-#     # python eval/vit/train.py --config eval/vit/config.py \
-#     #     --config.model.dropout.p=$i \
-#     #     --config.model.dropout.variant="blockwise[cuda]" \
-#     #     --config.data.name="cifar10" \
-#     #     --config.data.train_size=16364 \
-#     #     --config.seed=$seed
-# done
-
-for i in 0.4
+for i in 0 0.1 0.2 0.3 0.4 0.5 0.6 0.7
 do
+    python eval/vit/train.py --config eval/vit/config.py \
+        --config.model.dropout.p=$i \
+        --config.model.dropout.variant="vanilla" \
+        --config.data.name="cifar10" \
+        --config.data.train_size=16364 \
+        --config.seed=$seed
+
     python eval/vit/train.py --config eval/vit/config.py \
         --config.model.dropout.p=$i \
         --config.model.dropout.variant="blockwise[cuda]" \
         --config.data.name="cifar10" \
         --config.data.train_size=16364 \
-        --config.train.early_stop.patience=10 \
         --config.seed=$seed
 done
